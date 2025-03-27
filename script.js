8d0880bc37b50ba93a5b96334a75f09015a1f292
pawan pc <pawanhardasani8@gmail.com>
2025-03-27 15:19:45 +0530

    implemented string templete

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Pawan is a good boy");
+/*
+const firstName = "pawan";
+const sirName = "Hardasani";
+const bornYear = 2003;
+
+console.log (`My Name is ${firstName} 
+    My Sir name is ${sirName} 
+    My age is ${2025 - bornYear}`); */
