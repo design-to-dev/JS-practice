c5ad7d7bd9b3f9e7f099233b74ce2612f5b48237
pawan932 <142396193+pawan932@users.noreply.github.com>
2025-03-27 16:38:19 +0530

    if else applied

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const bornYear = 2003;
 console.log (`My Name is ${firstName} 
     My Sir name is ${sirName} 
     My age is ${2025 - bornYear}`); */
+    let anyaAge = 13
+    const ageRequired = 18
+
+    if (anyaAge >= ageRequired) {
+        console.log('Welcome to Essay Club')
+    } else {
+        console.log('jaldi paida hona chahiye thi bhai')
+    }
