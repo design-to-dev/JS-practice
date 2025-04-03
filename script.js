22f26722f49ec263dfbb11b92258ea1fd2b34650
pawan pc <pawanhardasani8@gmail.com>
2025-04-03 10:54:50 +0530

    Mark jonas mass calculate

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ const bornYear = 2003;
 
 console.log (`My Name is ${firstName} 
     My Sir name is ${sirName} 
-    My age is ${2025 - bornYear}`); */
-    let anyaAge = 13
-    const ageRequired = 18
+    My age is ${2025 - bornYear}`); 
+    let anyaAge = 19;
+    const ageRequired = 18;
 
     if (anyaAge >= ageRequired) {
         console.log('Welcome to Essay Club')
     } else {
         console.log('jaldi paida hona chahiye thi bhai')
     }
+        const massMark = 55;
+        const heightMark = 1.85;
+        const massJohn = 45;
+        const heightJohn = 2.35;
+        
+        const BMIMark = massMark / (heightMark * heightMark);
+        const BMIJohn = massJohn / (heightJohn * heightJohn);
+        console.log(BMIMark, BMIJohn);
+        if (BMIMark >= BMIJohn) {
+            console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+        } else {
+            console.log(`Mark's BMI ${BMIJohn} is higher than John's ${BMIMark}!`);
+        }*/
+        
+        
+    
