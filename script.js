afeecb5e49922fb6ffc73354c3f0ca850cb99cbf
pawan pc <pawanhardasani8@gmail.com>
2025-06-30 19:05:54 +0530

    Mujse dosti karoge

--- script.js
+++ script.js
@@ -1,210 +1,27 @@
-/*
-const firstName = "pawan";
-const sirName = "Hardasani";
-const bornYear = 2003;
-
-console.log (`My Name is ${firstName} 
-    My Sir name is ${sirName} 
-    My age is ${2025 - bornYear}`); 
-    let anyaAge = 19;
-    const ageRequired = 18;
-
-    if (anyaAge >= ageRequired) {
-        console.log('Welcome to Essay Club')
-    } else {
-        console.log('jaldi paida hona chahiye thi bhai')
-    }
-        const massMark = 55;
-        const heightMark = 1.85;
-        const massJohn = 45;
-        const heightJohn = 2.35;
-        
-        const BMIMark = massMark / (heightMark * heightMark);
-        const BMIJohn = massJohn / (heightJohn * heightJohn);
-        console.log(BMIMark, BMIJohn);
-        if (BMIMark >= BMIJohn) {
-            console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
-        } else {
-            console.log(`Mark's BMI ${BMIJohn} is higher than John's ${BMIMark}!`);
-        }
-            
-        const myAge = "23"
-        console.log(Number(myAge) + 25)
-        Truthy and falsy values*/
-//         let umarWife = ""
-//     if (umarWife) {
-//         console.log("itni to allowed he abhi chalta he");
-//     }
-//     else {
-// console.log("happy life");
-//     
-// const Dolphins1 = 96;
-// const Dolphins2 = 108;
-// const Dolphins3 = 110;
-
-// const Koalas1 = 88;
-// const Koalas2 = 91;
-// const Koalas3 = 89;
-
-// let scoreDolphins = (Dolphins1 + Dolphins2 + Dolphins3) / 3;
-// let scoreKoalas = (Koalas1 + Koalas2 + Koalas3) / 3;
-// console.log(scoreDolphins)
-// console.log(scoreKoalas)
-
-// if (scoreDolphins > scoreKoalas) {
-//     console.log("Dolphins win the trophy");
-// } else if (scoreDolphins < scoreKoalas){
-//      console.log("Dolphins win the trophy");
-// } else if (scoreDolphins === scoreKoalas) {
-//     scoreDolphins === scoreKoalas
-// } else{}
-
-// function personalDetails(firstName, lastName) {
-//     console.log(`Hello Guyz My Name is ${firstName} ${lastName}`)
-// }
-
-// let name = "Pawan"
-// let sirName = "Hardasani"
-// personalDetails(name, sirName)
-
-// function juiceMakerMachine(apple, mango, banana){
-// const juice = `Juice of ${apple} Apples and ${mango} Mangoes ${banana} Bananas is ready`;
-// return juice;
-// }
-// const finalJuice = juiceMakerMachine(2, 5, 5);
-// console.log(finalJuice);
-
-// function multiplyer(num) {
-//     const number =(num * num);
-//     return number;
-// }
-
-// const userinput = prompt("Enter a Number");
-// let finalnum = multiplyer(Number(userinput));
-// document.write(finalnum);
-
-// const agecalc = function(birthyear){
-//     const age = 2025 - birthyear;
-//     return age;
-// }
-// let age1 = agecalc(2003);
-// console.log(age1)
-// function cutFruitPieces(fruit){
-//     return fruit * 4;
-// }
-// function fruitJuiceMaker(mango, apple){
-//     const mangopices = cutFruitPieces(mango);
-//     const applepices = cutFruitPieces(apple);
-//     const juice = `Juice with ${mangopices} mangopices and ${applepices} applepieces are done`;
-//     return juice;
-// }
-
-//     const finalJuice = fruitJuiceMaker(5, 2)
-//     console.log(finalJuice)
-// const calcAverage = (a, b, c) => a + b + c / 3;
-// const scoreDolphins = calcAverage(44, 23, 71);
-// const scoreKoalas = calcAverage(85, 54, 49);
-
-// const winner = function(score1, score2){
-//     if (scoreDolphins >= scoreKoalas * 2) {
-//         console.log(`Team Score Dolphins Win ${scoreDolphins} vs ${scoreKoalas}`)
-//     } else if (scoreKoalas >= scoreDolphins * 2) {
-//         console.log(`Team Score Koalas Win ${scoreKoalas} vs ${scoreDolphins}`)
-//     } else{
-//         console.log("No ones Win")
-//     }
-// }
-// winner(scoreDolphins, scoreKoalas);
-// function calcTip(bill){
-//     let tip;
-//     if (bill >= 50 && bill >= 300) {
-//          tip = bill * 0.15
-//     } else {
-//         tip = bill * 0.20
-//     }
-//     return tip;
-// }
-// const tip = calcTip(100);
-// console.log(tip)
-// const jonas = {
-//   firstName: 'Jonas',
-//   lastName: 'Schmedtmann',
-//   birthYear: 2003,
-//   job: 'teacher',
-//   friends: ['Michael', 'Peter', 'Steven'],
-//   age : function() {
-//     return 2025 - this.birthYear
-//     console.log(jonas.age())
-//   }
-  
-// };
-// console.log(jonas.age())
-// // console.log(jonas);
-// // console.log(jonas.lastName);
-// // console.log(jonas['lastName']);
-
-// // const nameKey = 'Name';
-// // console.log(jonas['first' + nameKey]);
-// // console.log(jonas['last' + nameKey]);
-// // // console.log(jonas.'last' + nameKey)
-
-// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
-// console.log(jonas[interestedIn]);
-// .console.log();
-// let friends = ['Pawan','omar' , 'nithil']
-// friends[2] ="Babu"
-// console.log(friends);
-// const John = {
-//     fullName :'john lent',
-//     mass : 92,
-//     height : 1.95,
-//     calcBMI :function(){
-//         this.bmi = this.mass / (this.height ** 2)
-//         return this.bmi;
-//     }
-// }
-// //  console.log(John.calcBMI())
-//  console.log(John.bmi)
-// const bona = [
-//     "Utsav akhani", 
-//     25, 
-//     4.5,
-//     53
-// ];
-// for (let i = 0; i < bona.length; i++) {
-//     const element = bona[i];
-//     console.log(element);
-    
-// };
-// const name = "pratham"
-// let i = 1
-// while (i <= 13) {
-//     console.log(name);
-//     i++;
-// }
 var addButtons = document.querySelectorAll(".addfriendbutton");
-var statuses = document.querySelectorAll(".status");
 
-addButtons.forEach(function(button, index) {
-  let isFriend = false; // move inside loop — now unique per button
-
+addButtons.forEach(function(button) {
   button.addEventListener("click", function () {
-    if (!isFriend) {
-      statuses[index].innerHTML = "Friends";
+    var card = button.closest(".profile-card");
+    var status = card.querySelector(".status");
+
+    if (status.innerHTML.trim() === "Unknown") {
+      status.innerHTML = "Friends";
       button.innerHTML = "Remove Friend";
       button.classList.remove("btn-primary");
       button.classList.add("btn-danger");
-      isFriend = true;
+
+      // Show horror after 4 seconds
+      setTimeout(function () {
+        document.getElementById("killedOverlay").classList.remove("d-none");
+        document.getElementById("bloodEffect").classList.remove("d-none");
+        document.getElementById("horrorSound").play();
+      }, 4000);
     } else {
-      statuses[index].innerHTML = "Unknown";
+      status.innerHTML = "Unknown";
       button.innerHTML = "Add Friend";
       button.classList.remove("btn-danger");
       button.classList.add("btn-primary");
-      isFriend = false;
     }
   });
 });
-
-
-
-
